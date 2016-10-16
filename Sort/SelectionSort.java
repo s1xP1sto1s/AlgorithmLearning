bce12576060f1e2461ce12d761cc379d1327d181
s1xP1sto1s <1720461057@qq.com>
2016-10-16 15:55:32 +0800

    Add files via upload

---
+++ Sort/SelectionSort.java
@@ -0,0 +1,52 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+	public static void main(String[] args)
+	{
+		Integer[] arr = new Integer[]{5,3,9,4,1,3,12,445,78,345,66,123};
+		sort(arr);
+		//System.out.println(Arrays.toString(sort(arr)));
+		for(Integer tmp:arr)
+		{
+			System.out.print(tmp+" ");
+		}
+	}
+	//使用泛型和泛型限定
+	public static <T extends Comparable> void sort(T[] arr)
+	{
+		int len = arr.length;
+		for(int i=0;i<len-1;i++)
+		{
+			for(int j=i+1;j<len;j++)
+			{
+				if(arr[i].compareTo(arr[j])>0)
+				{
+					T temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+/*	
+ *应用多态特性
+	public static void sort(Comparable[] arr)
+	{
+		int len = arr.length;
+		for(int i=0;i<len-1;i++)
+		{
+			for(int j=i+1;j<len;j++)
+			{
+				if(arr[i].compareTo(arr[j])>0)
+				{
+					Comparable temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+*/
+}
